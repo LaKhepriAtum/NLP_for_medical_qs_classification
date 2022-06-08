3ee73105d4fb03b6f0be4001221534bd85541bd4
suchan <tncksrla96@gmail.com>
2022-06-08 15:56:28 +0900

    check_department

---
+++ code/job02_concat.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import glob
+data_paths = glob.glob('../datasets/*') # 데이터set 안의 모든 경로를 리스트로
+df = pd.DataFrame()
+for paths in data_paths:
+    paths = glob.glob(paths) # 데이터 set 안의 path
+    for path in paths:
+        path = glob.glob(path+'/*')
+        for i in path:
+            df_temp = pd.read_csv(i)
+            # df_temp = df_temp.reset_index()
+            df_temp.columns = ['title', 'content', 'department']
+            df = pd.concat([df, df_temp], axis='rows')
+
+
+print(df.head())
+df.info()
+# #
+df.to_csv('./datasets/medical_qs_Classification.csv', index = False)
--- job07_postprocessing_dropna.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-df = pd.read_csv('./datasets/medical_qs_all.csv')
-df = df.dropna(axis=0)
-df.info()
-df = df[['medical_qs', 'department']]
-df.info()
-df.to_csv('./datasets/medical_qs_all.csv', index = False)
